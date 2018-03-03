fc28e81d24d48f81c5ac03b763b0e7fe97140fe4
Carra78 <carra78@hotmail.com>
2018-03-03 17:19:56 +0000

    changes to default page & sitemap

---
+++ EllensBnB/Default.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EllensBnB
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			//Server.Transfer("~/Pages/Default");
+			Response.Redirect("/Pages/Default.aspx");
+		}
+	}
+}
